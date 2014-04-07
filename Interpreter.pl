16f208ac82fc57aec0d8d5b4befa71b73e4be19d
claraantolin <clara.antolin@hotmail.com>
2014-04-07 15:52:01 +0200

    Lee bien operadores boleanos

--- Interpreter.pl
+++ Interpreter.pl
@@ -187,17 +187,17 @@ resolveExpression(_,('const',[_=Value],_),Result):-
 %					---> Boolean <---
 %					-----------------
 
-work('=<', Op1,Op2, true):- Op1 #=< Op2.
-work('=<', _,_,false).
+work('&lt;=', Op1,Op2, true):- Op1 #=< Op2.
+work('&lt;=', _,_,false).
 
-work('<', Op1,Op2,true):- Op1 #< Op2.
-work('<', _,_,false).
+work('&lt;', Op1,Op2,true):- Op1 #< Op2.
+work('&lt;', _,_,false).
 
-work('>=', Op1,Op2,true):- Op1 #>= Op2.
-work('>=', _,_,false).
+work('&gt;=', Op1,Op2,true):- Op1 #>= Op2.
+work('&gt;=', _,_,false).
 
-work('>', Op1,Op2,true):- Op1 #> Op2.
-work('>', _,_,false).
+work('&gt;', Op1,Op2,true):- Op1 #> Op2.
+work('&gt;', _,_,false).
 
 work('==', Op1,Op2,true):- Op1 #= Op2.
 work('==', _,_,false).
