6238386f5171420b479f2b90d204e09aacd53690
Carlos Giraldo <cacu2ggg@gmail.com>
2014-05-25 17:30:43 +0200

    funciones basicas actualizadas

--- Examples/funcionesBasicas.cc
+++ Examples/funcionesBasicas.cc
@@ -44,7 +44,7 @@ int introduceEntero(){
 	return ConsoleIn_Int();
 }
 
-int escribeEntero(int a){
+int introduceEntero(int a){
 	ConsoleOut_Int(a);
 	return 0;
 }
@@ -113,20 +113,4 @@ int pruebaWhile01(int a){
 		r--;
 	}
 	return 0;
-}
-
-int pruebaWhile02(int a, int b){
-	int c = b;
-	int r = 1;
-	while(c>0){
-		r *= a;
-		c--;
-	}
-	return r;
-}
-
-int llamadaAFuncion(int a){
-	int r;
-	r = factorial(a);
-	return r;
 }
